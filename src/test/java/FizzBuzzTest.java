67f57a873f3fb964b36b5cc918cd59ae28165f2a
Nyamgerel12 <zgerlee12@gmail.com>
2026-09-22 17:58:16 +0200

    step1: (rot): Fehlgeschlagener Test fuer normale Zahl-Nyamgerel

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    @Test 
+    void convert_regularNumber_returnsNumberAsString(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1", fizzBuzz.convert(1));
+    }
+}
